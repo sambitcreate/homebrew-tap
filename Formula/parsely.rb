dc2e11e8905c8ad7d0aaea42623d68cdef74c498
Sambit <sambit.biswas2@gmail.com>
2026-02-02 12:07:07 -0500

    Add npm URL and sha256 hash

--- Formula/parsely.rb
+++ Formula/parsely.rb
@@ -3,6 +3,9 @@
 class Parsely < Formula
   desc "Smart recipe scraper CLI with interactive TUI built on Ink"
   homepage "https://github.com/sambitcreate/parsely-cli"
+  url "https://registry.npmjs.org/@sambitcreate/parsely-cli/-/parsely-cli-2.0.0.tgz"
+  sha256 "e51ba9198907ff18871161ac1b6f711fc285be0a84f1af7d3fff06523264ac35"
+  license "MIT"
 
   depends_on "node"
 
