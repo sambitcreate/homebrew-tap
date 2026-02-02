9a5f434f71ede2763a17569beeadb202be01edfc
Sambit <sambit.biswas2@gmail.com>
2026-02-02 12:06:02 -0500

    Simplify formula to install from npm without git cloning

--- Formula/parsely.rb
+++ Formula/parsely.rb
@@ -3,15 +3,11 @@
 class Parsely < Formula
   desc "Smart recipe scraper CLI with interactive TUI built on Ink"
   homepage "https://github.com/sambitcreate/parsely-cli"
-  url "https://github.com/sambitcreate/parsely-cli.git",
-      tag:      "v2.0.0",
-      revision: "main"
-  head "https://github.com/sambitcreate/parsely-cli.git", branch: "main"
 
   depends_on "node"
 
   def install
-    system "npm", "install", "-g", "@sambitcreate/parsely-cli@2.0.0", *std_npm_install_args
+    system "npm", "install", "-g", "@sambitcreate/parsely-cli", *std_npm_install_args
   end
 
   test do
