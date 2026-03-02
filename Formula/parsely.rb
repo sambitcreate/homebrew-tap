8f7d6bf0526a59b538508ae1c74aafd71e859238
Sambit Biswas <sambit.biswas2@gmail.com>
2026-03-02 13:47:41 -0500

    parsely 2.1.0

--- Formula/parsely.rb
+++ Formula/parsely.rb
@@ -3,8 +3,8 @@
 class Parsely < Formula
   desc "Smart recipe scraper CLI with interactive TUI built on Ink"
   homepage "https://github.com/sambitcreate/parsely-cli"
-  url "https://registry.npmjs.org/@sambitcreate/parsely-cli/-/parsely-cli-2.0.0.tgz"
-  sha256 "e51ba9198907ff18871161ac1b6f711fc285be0a84f1af7d3fff06523264ac35"
+  url "https://registry.npmjs.org/@sambitcreate/parsely-cli/-/parsely-cli-2.1.0.tgz"
+  sha256 "76f18591e3b3af0547c5e264e830d06d329f32cbe49b002d9883305959ce6770"
   license "MIT"
 
   depends_on "node"
